eafcbfa135214a8967a00986df4dc20201a5e19b
Andrey Helldar <helldar@ai-rus.com>
2021-05-25 19:22:26 +0300

    The division of the project into drivers has been prepared

---
+++ src/Helpers/Statuses.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Helldar\CashierTinkoff\Helpers;
+
+use Helldar\Cashier\Helpers\Statuses as BaseStatus;
+
+class Statuses extends BaseStatus
+{
+    public const NEW = [
+        'NEW',
+    ];
+
+    public const REFUNDING = [
+        'AUTHORIZING',
+        'AUTHORIZED',
+        'CONFIRMING',
+        'CONFIRMED',
+        'REFUNDING',
+    ];
+
+    public const REFUNDED = [];
+
+    public const FAILED = [];
+}
