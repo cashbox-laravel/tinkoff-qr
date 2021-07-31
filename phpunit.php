0328adff2b2d9f892868b88d9c66324f1a7bbf1e
Andrey Helldar <helldar@ai-rus.com>
2021-08-01 02:05:25 +0300

    Added copyrights

--- phpunit.php
+++ phpunit.php
@@ -1,6 +1,21 @@
 <?php
 
 /*
+ * This file is part of the "andrey-helldar/cashier-tinkoff-qr" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/andrey-helldar/cashier-tinkoff-qr
+ */
+
+/*
   |--------------------------------------------------------------------------
   | Register The Composer Auto Loader
   |--------------------------------------------------------------------------
